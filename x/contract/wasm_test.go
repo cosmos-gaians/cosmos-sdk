ca8891d10e8b50f654a01fde5960a123e55fdaba
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-15 20:31:23 +0200

    Improve tests

--- x/contract/wasm_test.go
+++ x/contract/wasm_test.go
@@ -29,7 +29,7 @@ func TestRegenInit(t *testing.T) {
 	// Set up global static for test
 	data = "{}"
 
-	json := `{
+	initMsg := `{
 		"sender": "0123456789",
 		"init_funds": 1000,
 		"init_msg": {
@@ -38,11 +38,55 @@ func TestRegenInit(t *testing.T) {
 		}
 	}`
 
-	res, err := Run(regen, "init", []interface{}{json}, AsString)
+	res, err := Run(regen, "init", []interface{}{initMsg}, AsString)
 	if err != nil {
 		t.Fatalf("%+v", err)
 	}
 	if res.(string) != "" {
 		t.Fatalf("Unexpected result: %d", res)
 	}
+
+	badSend := `{
+		invalid: 123
+	}`
+
+	res, err = Run(regen, "send", []interface{}{badSend}, AsString)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	// Returns "Could not parse MsgSendContract json." on parse error
+	if res.(string) != "Could not parse MsgSendContract json." {
+		t.Fatalf("Unexpected result: %d", res)
+	}
+
+	unauthSend := `{
+		"sender": "0123456789",
+		"payment": 20,
+		"msg": "TODO"
+	}`
+
+	res, err = Run(regen, "send", []interface{}{unauthSend}, AsString)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	// Returns "" on error
+	if res.(string) != "" {
+		t.Fatalf("Unexpected result: %d", res)
+	}
+
+	goodSend := `{
+		"sender": "ethan",
+		"payment": 20,
+		"msg": "TODO"
+	}`
+
+	res, err = Run(regen, "send", []interface{}{goodSend}, AsString)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	// This is placeholder on success
+	if res.(string) != "Send tx goes here !!!" {
+		t.Fatalf("Unexpected result: %d", res)
+	}
+
 }
