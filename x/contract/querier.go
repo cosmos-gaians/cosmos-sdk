1779fa80620489cc6c7d175bb21306d6eb03a7fb
Aaron Craelius <aaron@regen.network>
2019-06-16 13:41:17 +0200

    Add contract state querier

--- x/contract/querier.go
+++ x/contract/querier.go
@@ -15,13 +15,18 @@ func NewQuerier(keeper Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, sdk.Error) {
 		switch path[0] {
 		case QueryGetState:
-			return queryContractState(ctx, path[1:], req, keeper)
+			return queryContractState(ctx, path[1], req, keeper)
 		default:
 			return nil, sdk.ErrUnknownRequest("unknown data query endpoint")
 		}
 	}
 }
 
-func queryContractState(ctx sdk.Context, path []string, req abci.RequestQuery, keeper Keeper) (res []byte, err sdk.Error) {
+func queryContractState(ctx sdk.Context, bech string, req abci.RequestQuery, keeper Keeper) (res []byte, err sdk.Error) {
+	addr, e := sdk.AccAddressFromBech32(bech)
+	if e != nil {
+		return nil, sdk.ErrUnknownRequest(e.Error())
+	}
+	res = keeper.GetContractState(ctx, addr)
 	return res, nil
 }
