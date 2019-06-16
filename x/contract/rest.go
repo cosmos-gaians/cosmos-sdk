40bf01112b9aa162900f578e7a042bfc5dd3d76d
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-06-16 14:23:01 +0200

    Contract rest endpoints working

--- x/contract/rest.go
+++ x/contract/rest.go
@@ -2,10 +2,11 @@ package contract
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/gorilla/mux"
-	"net/http"
 )
 
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
@@ -14,8 +15,12 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
 
 func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	r.HandleFunc(
-		"/contract/state/{addr}",
+		"/contracts/state/{addr}",
 		contractStateHandlerFn(cliCtx),
+	).Methods("GET")
+	r.HandleFunc(
+		"/contracts/list",
+		contractListHandlerFn(cliCtx),
 	).Methods("GET")
 }
 
@@ -24,7 +29,22 @@ func contractStateHandlerFn(cliContext context.CLIContext) func(http.ResponseWri
 		vars := mux.Vars(r)
 		addr := vars["addr"]
 		route := fmt.Sprintf("custom/%s/%s/%s", "contract", "state", addr)
+		fmt.Println(route)
 
+		res, err := cliContext.QueryWithData(route, nil)
+		if err != nil {
+			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		rest.PostProcessResponse(w, cliContext, res)
+	}
+}
+
+func contractListHandlerFn(cliContext context.CLIContext) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		route := fmt.Sprintf("custom/contract/list/abc")
+		fmt.Println(route)
 
 		res, err := cliContext.QueryWithData(route, nil)
 		if err != nil {
